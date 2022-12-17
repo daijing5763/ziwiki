57349c215f47010d629095399c89565e1d07e7fc
wangxili <wangxili@tsingj.com>
2022-12-17 13:11:00 +0800

    update

--- fronted/components/popnav.tsx
+++ fronted/components/popnav.tsx
@@ -149,7 +149,7 @@ return (
             >
             <div className="absolute z-20 top-0 inset-x-0 flex justify-center overflow-hidden pointer-events-none">
                 <div className="w-[108rem] flex-none flex justify-end">
-                    <img src="bg.avif" alt="" className="w-[90rem] flex-none max-w-none  block" decoding="async" />
+                    <img src="/bg.avif" alt="" className="w-[90rem] flex-none max-w-none  block" decoding="async" />
                 </div>
             </div>
             <div className="overflow-hidden 
