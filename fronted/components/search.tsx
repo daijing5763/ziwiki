081048e6578b5cadd3010067c640b5f3dac3b20f
wangxili <wangxili@tsingj.com>
2022-12-20 22:49:24 +0800

    update

--- fronted/components/search.tsx
+++ fronted/components/search.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React, { useState, useEffect, useRef, useCallback } from 'react';
 import ReactDOM  from 'react-dom';
 import {MdSearch, MdCancel} from "react-icons/md"
-import { CgHashtag } from "react-icons/cg"
+import { CgHashtag,CgSearchLoading } from "react-icons/cg"
 import { BiChevronRight } from "react-icons/bi"
 import parse, { domToReact } from 'html-react-parser';
 import { fetch_markdown_user } from "../utils/web_fetch"
@@ -10,11 +10,17 @@ import { useSession } from "next-auth/react"
 function Search({ useSearch, setUseSearch }) {
   const { data: session } = useSession()
   const [query, setquery] = useState('');
+  const [loading, setloading] = useState(false);
   const [searchedDoc, setSearchedDoc] = useState([])
   async function queryMarkdownUser(values: { [key: string]: string }) {
+    setloading(true)
     fetch_markdown_user(values, session.access_token).then(data => {
+      setloading(false)
       if (data && !data.error) {
+        console.log(data)
         setSearchedDoc(data)
+      } else {
+        setSearchedDoc([])
       }
     })
   }
@@ -30,9 +36,13 @@ function Search({ useSearch, setUseSearch }) {
   };
 
   const submitContact = async (event) => {
-    event.preventDefault();
-    setquery(event.target.search_query.value)
-    setSearchedDoc([])
+    event.preventDefault();// 阻止redirect to search query
+    if (event.target.value) {
+      setquery(event.target.value)
+    } else {
+      setquery('')
+      setSearchedDoc([])
+    }
   };
   useEffect(() => {  document.addEventListener('click', onClickOutsideHandler);},[])
   const bodyBox = useRef(null);
@@ -66,20 +76,20 @@ function Search({ useSearch, setUseSearch }) {
       
           <div ref={bodyBox} className="flex flex-col  border-slate-200/5 dark:border-slate-800/75 border rounded-lg bg-white/75 dark:bg-slate-800/75" >
             <header className="px-4 py-4 relative flex text-slate-500 flex-row items-center border-b border-slate-300/75 dark:border-slate-800/75">
-              <form onSubmit={submitContact} className="flex flex-grow flex-shrink items-center">
-                <button type="submit">
-                  <MdSearch className="w-7 h-7" />
-                </button>
-                <input className="px-4 py-2 flex flex-grow flex-shrink focus:ring-0 focus-within:text-gray-600 bg-transparent placeholder-slate-400 text-gray-900 dark:text-slate-300 appearance-none w-full  border-0  focus:outline-none" id="search_query"
-                  type="text"
-                  name="search_query" placeholder="搜索文档" />
+            <form onChange={submitContact} onSubmit={submitContact} className="flex flex-grow flex-shrink items-center">
+              <MdSearch className={`${loading && "hidden"} w-7 h-7`} />
+              <CgSearchLoading className={`${!loading && "hidden"} w-6 h-6`} />
+              <input className="px-4 py-2 flex flex-grow flex-shrink focus:ring-0 focus-within:text-slate-900 bg-transparent placeholder-slate-400 text-gray-900 dark:text-slate-300 appearance-none w-full  border-0  focus:outline-none" id="search_query"
+                type="text"
+                name="search_query" placeholder="搜索文档" />
               </form>
               <button onClick={clearquery}><MdCancel className="w-7 h-7 px-1.5 py-1" /></button>
             </header>
 
             <div className="overflow-y-auto h-80  scrollbar-thin   scrollbar-thumb-rounded-md scrollbar-track-rounded-md">
-              <div className="">
-                {searchedDoc.length == 0 && <div className="flex h-40 text-semibold text-slate-700 dark:text-slate-200 items-center justify-center">无历史搜索</div>}
+            <div className="">
+              {searchedDoc.length == 0 && query.length>0 && <div className="flex h-40 text-semibold text-slate-500 dark:text-slate-400 items-center justify-center">未找到包含 <span className='dark:text-slate-50 text-slate-800'>{query}</span> 的相关文档</div>}
+              {searchedDoc.length == 0 && query.length==0 &&<div className="flex h-40 text-semibold text-slate-500 dark:text-slate-400 items-center justify-center">对用户所有仓库全文进行检索</div>}
                 <ul className="my-4">
                   {searchedDoc.length > 0 && searchedDoc.map((doc, index) => (
                     <li key={index} className="flex items-center mb-2">
@@ -103,7 +113,6 @@ function Search({ useSearch, setUseSearch }) {
                     </li>
                   ))}
                 </ul>
-
               </div>
             </div>
 
