b87deb813d4cb79b9a8761951837a8a5ba80b839
wangxili <wangxili@tsingj.com>
2022-12-29 22:15:51 +0800

    update

--- dev_compose.sh
+++ dev_compose.sh
@@ -1,3 +1,3 @@
 docker-compose --env-file dev.env -f dev-docker-compose.yaml down --volumes
-docker-compose --env-file dev.env -f dev-docker-compose.yaml up
-
+docker-compose --env-file dev.env -f dev-docker-compose.yaml build
+docker-compose --env-file dev.env -f dev-docker-compose.yaml up 
--- prod_compose.sh
+++ prod_compose.sh
@@ -1,2 +1,3 @@
 docker-compose --env-file prod.env -f prod-docker-compose.yaml down --volumes
+docker-compose --env-file prod.env -f prod-docker-compose.yaml build
 docker-compose --env-file prod.env -f prod-docker-compose.yaml up 
