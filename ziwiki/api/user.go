6faabc009b5c1961ba640d8ac1b1942c66d90701
wangxili <wangxili@tsingj.com>
2022-12-30 20:00:45 +0800

    update

--- ziwiki/api/user.go
+++ ziwiki/api/user.go
@@ -112,8 +112,9 @@ func (server *Server) loginUser(ctx *gin.Context) {
 		return
 	}
 	ip := ctx.ClientIP()
-	ctx.String(200, ip)
-	fmt.Println("login user ip is:", ip)
+	fmt.Println("ip is:", ip)
+	// ctx.String(200, ip)
+	// fmt.Println("login user ip is:", ip)
 	user, err := server.store.GetUser(ctx, req.Username)
 	if err != nil {
 		if err == sql.ErrNoRows {
